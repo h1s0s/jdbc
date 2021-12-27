56847a419a3d2d138216ec8945c37225e8b6dad5
한상선 <hs94122@naver.com>
2021-12-27 15:34:17 +0900

    2021-12-27 15:34

--- src/com/javaex/ex01/AuthorInsert.java
+++ src/com/javaex/ex01/AuthorInsert.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class AuthorApp {
+public class AuthorInsert {
 
 	public static void main(String[] args) {
 		
