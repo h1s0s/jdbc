a904dbabe4084ce6a8d0cc98cf3b95505bf258b5
한상선 <hs94122@naver.com>
2021-12-29 14:45:41 +0900

    2021-12-29 14:45

--- src/com/javaex/ex02/AuthorApp.java
+++ src/com/javaex/ex02/AuthorApp.java
@@ -7,6 +7,7 @@ public class AuthorApp {
 	public static void main(String[] args) {
 		//AuthorDao라는 클래스에 DML 메소드를 만들어 사용하기
 		
+		List<AuthorVo> authorList;
 		AuthorDao authorDao = new AuthorDao();
 		
 		//작가 등록(insert)
@@ -18,14 +19,21 @@ public class AuthorApp {
 		//작가 등록(insert)
 		authorDao.authorInsert("유시민", "17대 국회의원");
 		
-		List<AuthorVo> authorList = authorDao.authorSelect();
+		authorList = authorDao.authorSelect();
 		for(int i=0; i<authorList.size(); i++) {
 			AuthorVo vo = authorList.get(i);
 			System.out.println(vo.getAuthorId() + ", " + vo.getAuthorName() + ", " + vo.getAuthorDesc());
 		}
+		System.out.println("--------------------------------------------------------------------------");
 		
 		//작가 수정(Update)
-		//authorDao.authorUpdate(2, "박경리(수정)", "경상남도 통영(수정)");
+		authorDao.authorUpdate(2, "박경리(수정)", "경상남도 통영(수정)");
+		
+		authorList = authorDao.authorSelect();
+		for(int i=0; i<authorList.size(); i++) {
+			AuthorVo vo = authorList.get(i);
+			System.out.println(vo.getAuthorId() + ", " + vo.getAuthorName() + ", " + vo.getAuthorDesc());
+		}
 		
 		//작가 삭제(Delete)
 		//authorDao.authorDelete(1);
